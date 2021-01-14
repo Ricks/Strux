6452a3f4c40c99d36d29e54d7db0ac013628cc84
Rick <rick@scipioapps.com>
2021-01-13 17:01:22 -0800

    Added DisjointSet.

---
+++ Sources/Strux/DisjointSet.swift
@@ -0,0 +1,102 @@
+//
+//  DisjointSet.swift
+//  Strux
+//
+//  Created by Richard Clark on 1/11/21.
+//  Copyright © 2020 Richard Clark. All rights reserved.
+//  MIT License (see LICENSE file).
+//
+
+import Foundation
+
+/// A disjoint-set data structure, also known as union-find or merge-find data structure. The data structure stores
+/// associations between unique values of type T. Two values are in the same subset if there are associations that
+/// link them. The structure allows you to quickly determine whether two values are in the same subset.
+///
+/// There are two methods used to set the associations and determine whether two values are in the same subset:
+///    union(v1, v2) merges the subsets containing v1 and v2, if they're not already in the same subset
+///    find(v) returns the root value of the subset. Two values v1 and v2 are in the same subset if find(v1) = find(v2)
+///
+/// From Wikipedia: While there are several ways of implementing disjoint-set data structures, in practice they are
+/// often identified with a particular implementation called a disjoint-set forest. This is a specialized type of
+/// forest which performs unions and finds in near constant amortized time. To perform a sequence of m addition,
+/// union, or find operations on a disjoint-set forest with n nodes requires total time O(mα(n)), where α(n) is the
+/// extremely slow-growing inverse Ackermann function. Disjoint-set forests do not guarantee this performance on a
+/// per-operation basis. Individual union and find operations can take longer than a constant times α(n) time, but
+/// each operation causes the disjoint-set forest to adjust itself so that successive operations are faster.
+/// Disjoint-set forests are both asymptotically optimal and practically efficient.
+class DisjointSet<T: Hashable>: CustomStringConvertible {
+
+    private class Node<T: Hashable> {
+        let value: T
+        var size: Int
+        var parent: Node<T>?   // Root nodes have nil parent
+
+        init(_ value: T) {
+            self.value = value
+            size = 1
+        }
+    }
+
+    private var nodes = [T: Node<T>]()
+
+    private func getNode(_ value: T) -> Node<T> {
+        var node = nodes[value]
+        if node == nil {
+            node = Node(value)
+            nodes[value] = node
+        }
+        return node!
+    }
+
+    private func findNode(_ node: Node<T>) -> Node<T> {
+        if let parent = node.parent {
+            let root = findNode(parent)
+            node.parent = root
+            return root
+        }
+        return node
+    }
+
+    /************************** Public interface *************************/
+
+    /// Put two values in the same subset (make them have the same root).
+    /// - Parameter value1: first value
+    /// - Parameter value2: second value
+    public func union(_ value1: T, _ value2: T) {
+        let root1 = findNode(getNode(value1))
+        let root2 = findNode(getNode(value2))
+        guard root1 !== root2 else {
+            return
+        }
+        if root1.size > root2.size {
+            root2.parent = root1
+            root1.size += root2.size
+        } else {
+            root1.parent = root2
+            root2.size += root1.size
+        }
+    }
+
+    /// Return the root value of the given value
+    /// - Parameter value: value to find the root of
+    /// - Returns: the root of the value
+    public func find(_ value: T) -> T {
+        if let node = nodes[value] {
+            return findNode(node).value
+        }
+        return value
+    }
+
+    /// Clear the set, i.e. remove all associations and make it like a newly-created DisjointSet.
+    public func clear() {
+        nodes = [T: Node<T>]()
+    }
+
+    /// A string that is a newline-separated list of "<value>: root = find(<value>)", giving for each value in
+    /// the set, the root of the value. Only values that have been given as one of the values in a union()
+    /// call are listed.
+    public var description: String {
+        nodes.keys.map { "\($0): root = \(find($0))" }.joined(separator: "\n")
+    }
+}
---
+++ Tests/StruxTests/DisjointSetTests.swift
@@ -0,0 +1,109 @@
+//
+//  DisjointSetTests.swift
+//  
+//
+//  Created by Richard Clark on 1/11/21.
+//  Copyright © 2020 Richard Clark. All rights reserved.
+//  MIT License (see LICENSE file).
+//
+
+import XCTest
+@testable import Strux
+
+class Solution {
+
+    func distanceLimitedPathsExist(_ n: Int, _ edgeList: [[Int]], _ queries: [[Int]]) -> [Bool] {
+        // Sort edges, queries by distance
+        let sortedEdges = edgeList.sorted() { $0[2] < $1[2] }
+        let sortedQueries = queries.sorted() { $0[2] < $1[2] }
+        let ds = DisjointSet<Int>()
+        var resultDict = [[Int]: Bool]()
+
+        var edgeIndex = 0
+        for query in sortedQueries {
+            while edgeIndex < sortedEdges.count && sortedEdges[edgeIndex][2] < query[2] {
+                ds.union(sortedEdges[edgeIndex][0], sortedEdges[edgeIndex][1])
+                edgeIndex += 1
+            }
+            let result = ds.find(query[0]) == ds.find(query[1])
+            resultDict[query] = result
+        }
+
+        var results = [Bool]()
+        for query in queries {
+            results.append(resultDict[query] ?? false)
+        }
+        return results
+    }
+
+}
+
+class DisjointSetTests: XCTestCase {
+
+    func testDisjointSet1() {
+        let ds = DisjointSet<Int>()
+        ds.union(2, 3)
+        ds.union(3, 4)
+        XCTAssertNotEqual(ds.find(1), ds.find(2))
+        XCTAssertEqual(ds.find(2), ds.find(3))
+        XCTAssertEqual(ds.find(3), ds.find(4))
+        XCTAssertNotEqual(ds.find(4), ds.find(5))
+
+        var expectedLines = Set<String>()
+        for i in 2 ... 4 {
+            expectedLines.insert("\(i): root = \(ds.find(i))")
+        }
+        let actualLines = Set("\(ds)".split(separator: "\n").map { String($0) })
+        XCTAssertEqual(expectedLines, actualLines)
+    }
+
+    func testDisjointSet2() {
+        let ds = DisjointSet<Int>()
+        ds.union(1, 2)
+        ds.union(2, 3)
+        ds.union(3, 2)
+        ds.union(3, 4)
+        ds.union(4, 5)
+        XCTAssertEqual(ds.find(1), ds.find(2))
+        XCTAssertEqual(ds.find(2), ds.find(3))
+        XCTAssertEqual(ds.find(3), ds.find(4))
+        XCTAssertEqual(ds.find(4), ds.find(5))
+    }
+
+    func testClear() {
+        let ds = DisjointSet<Int>()
+        ds.union(1, 2)
+        ds.union(2, 3)
+        ds.union(3, 4)
+        ds.union(4, 5)
+
+        ds.clear()
+
+        XCTAssertEqual("\(ds)", "")
+        XCTAssertNotEqual(ds.find(1), ds.find(2))
+        XCTAssertNotEqual(ds.find(2), ds.find(3))
+        XCTAssertNotEqual(ds.find(3), ds.find(4))
+        XCTAssertNotEqual(ds.find(4), ds.find(5))
+        ds.union(1, 2)
+        ds.union(2, 3)
+        ds.union(3, 4)
+        ds.union(4, 5)
+        XCTAssertEqual(ds.find(1), ds.find(2))
+        XCTAssertEqual(ds.find(2), ds.find(3))
+        XCTAssertEqual(ds.find(3), ds.find(4))
+        XCTAssertEqual(ds.find(4), ds.find(5))
+    }
+
+    func testDistanceLimitedPathsExist() {
+        let sol = Solution()
+        XCTAssertEqual(sol.distanceLimitedPathsExist(3, [[0,1,2],[1,2,4],[2,0,8],[1,0,16]], [[0,1,2],[0,2,5]]),
+                       [false,true])
+        XCTAssertEqual(sol.distanceLimitedPathsExist(5, [[0,1,10],[1,2,5],[2,3,9],[3,4,13]], [[0,4,14],[1,4,13]]),
+                       [true,false])
+        XCTAssertEqual(sol.distanceLimitedPathsExist(13,
+                                                     [[9,1,53],[3,2,66],[12,5,99],[9,7,26],[1,4,78],[11,1,62],[3,10,50],[12,1,71],[12,6,63],[1,10,63],[9,10,88],[9,11,59],[1,4,37],[4,2,63],[0,2,26],[6,12,98],[9,11,99],[4,5,40],[2,8,25],[4,2,35],[8,10,9],[11,9,25],[10,11,11],[7,6,89],[2,4,99],[10,4,63]],
+                                                     [[9,7,65],[9,6,1],[4,5,34],[10,8,43],[3,7,76],[4,2,15],[7,6,52],[2,0,50],[7,6,62],[1,0,81],[4,5,35],[0,11,86],[12,5,50],[11,2,2],[9,5,6],[12,0,95],[10,6,9],[9,4,73],[6,10,48],[12,0,91],[9,10,58],[9,8,73],[2,3,44],[7,11,83],[5,3,14],[6,2,33]]),
+                       [true,false,false,true,true,false,false,true,false,true,false,true,false,false,false,true,false,true,false,true,true,true,false,true,false,false])
+    }
+
+}
