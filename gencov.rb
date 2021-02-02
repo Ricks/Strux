69ceca7240f9cb4fea9a9cf1ff152da50f8afd27
Rick <rick@scipioapps.com>
2021-02-02 14:08:58 -0800

    Coverage badge link (HTML/CSS) looks good.

--- gencov.rb
+++ gencov.rb
@@ -11,13 +11,37 @@ if !!!buildMatch
 end
 
 result_file = buildMatch[:file]
-text_file = "/tmp/codecov.txt"
+text_file = "/tmp/codecov.html"
 badge_file = "/tmp/codecov.svg"
 
 # Generate the coverage report
 
 #system "xcrun xccov view --report #{result_file} > #{text_file}"
 #covOut = File.read(text_file)
+
+pre_body = <<-PRE_BODY
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+/*  width: max-content; */
+  text-align: center;
+  background-color: linen;
+}
+</style>
+</head>
+<body>
+<pre>
+PRE_BODY
+
+post_body = <<-POST_BODY
+
+</pre>
+</body>
+</html>
+POST_BODY
+
 covOut = `xcrun xccov view --report #{result_file}`
 covMatch = covOut.match(/(^.*\n.*\n)([\S\s]*)/)
 firstTwoLines = covMatch[1]
@@ -33,10 +57,14 @@ targets.each do | target |
     targetName = targetMatch[:name]
     if targetName == "Strux.framework"
         pct = targetMatch[:coverage]
-        covFinal = "<pre>\n#{firstTwoLines}#{target}</pre>"
+        covFinal = "#{pre_body}#{firstTwoLines}#{target}#{post_body}"
         File.write(text_file, covFinal)
         break
     end
+end
+if pct == ""
+    print "Unable to parse coverage output!\n"
+    exit 1
 end
 
 # Get the badge color
@@ -57,7 +85,7 @@ end
 
 system "curl \"https://img.shields.io/badge/coverage-#{pct}%25-#{color}\" > #{badge_file}"
 system "gsutil rm gs://scipioapps/mystrux/codecov.svg"
-system "gsutil -h \"Cache-Control: no-cache\" cp #{text_file} gs://scipioapps/mystrux/codecov.txt"
+system "gsutil -h \"Cache-Control: no-cache\" cp #{text_file} gs://scipioapps/mystrux/codecov.html"
 system "gsutil -h \"Cache-Control: no-cache\" cp #{badge_file} gs://scipioapps/mystrux/codecov.svg"
-system "gsutil acl ch -u AllUsers:R gs://scipioapps/mystrux/codecov.txt"
+system "gsutil acl ch -u AllUsers:R gs://scipioapps/mystrux/codecov.html"
 system "gsutil acl ch -u AllUsers:R gs://scipioapps/mystrux/codecov.svg"
