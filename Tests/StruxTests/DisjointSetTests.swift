c2018eff928bda271ed2551b9360fe2b050e924d
Rick Clark <himself@rickclark.com>
2025-03-22 22:25:39 -0700

    Tweak

--- Tests/StruxTests/DisjointSetTests.swift
+++ Tests/StruxTests/DisjointSetTests.swift
@@ -17,28 +17,28 @@ class DisjointSetTests: XCTestCase {
         XCTAssertTrue(ds.isEmpty)
         XCTAssertEqual(ds.description, "Empty")
 
-        ds.insert(42)
+        ds.insert(9)
         XCTAssertEqual(ds.valueCount, 1)
         XCTAssertEqual(ds.subsetCount, 1)
         XCTAssertFalse(ds.isEmpty)
-        let desc1 = "1 subset, 1 value total\nsubset: 42"
+        let desc1 = "1 subset, 1 value total\nsubset: 9"
         XCTAssertEqual(ds.description, desc1)
 
         ds.insert(58)
         XCTAssertEqual(ds.valueCount, 2)
         XCTAssertEqual(ds.subsetCount, 2)
         XCTAssertFalse(ds.isEmpty)
-        let desc2 = "2 subsets, 2 values total\nsubset: 42\nsubset: 58"
+        let desc2 = "2 subsets, 2 values total\nsubset: 9\nsubset: 58"
         XCTAssertEqual(ds.description, desc2)
-        XCTAssertFalse(ds.inSameSubset(42, 58))
+        XCTAssertFalse(ds.inSameSubset(9, 58))
 
-        ds.merge(58, 42)
+        ds.merge(58, 9)
         XCTAssertEqual(ds.valueCount, 2)
         XCTAssertEqual(ds.subsetCount, 1)
         XCTAssertFalse(ds.isEmpty)
-        let desc3 = "1 subset, 2 values total\nsubset: 42, 58"
+        let desc3 = "1 subset, 2 values total\nsubset: 9, 58"
         XCTAssertEqual(ds.description, desc3)
-        XCTAssertTrue(ds.inSameSubset(42, 58))
+        XCTAssertTrue(ds.inSameSubset(9, 58))
     }
     
     // Add a new value by putting it at the beginning, middle, and end of a trio of values passed to merge()
