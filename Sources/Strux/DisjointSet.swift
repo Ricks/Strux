464b78373a39a22414885aa7ea034445d1d514e3
Rick Clark <himself@rickclark.com>
2025-03-25 08:39:44 -0700

    Disjoint set no longer T:Comparable, but description is different depending on whether T is Comparable

--- Sources/Strux/DisjointSet.swift
+++ Sources/Strux/DisjointSet.swift
@@ -7,28 +7,13 @@
 
 import Foundation
 
-struct DisjointSet<T: Hashable & Comparable>: CustomStringConvertible {
+struct DisjointSet<T: Hashable>: CustomStringConvertible {
     private var subsetDict = [Int: Set<T>]()
     private var keyDict = [T: Int]()
     private var maxKey = 0
 
     var description: String {
-        var description = ""
-        let subs = subsets
-        if subs.count == 0 {
-            description = "Empty"
-        } else {
-            description = subs.count == 1 ? "1 subset, " : "\(subs.count) subsets, "
-            description += valueCount == 1 ? "1 value total\n" : "\(valueCount) values total\n"
-            for i in 0 ..< subs.count {
-                description += "subset: "
-                description += subs[i].sorted().map { "\($0)" }.joined(separator: ", ")
-                 if i < subs.count - 1 {
-                    description += "\n"
-                }
-            }
-        }
-        return description
+        (self as? any DisjointSetOfComparableConvertible)?.customDescription ?? self.normalDescription
     }
 
     public func subset(with value: T) -> Set<T>? {
@@ -102,19 +87,12 @@ struct DisjointSet<T: Hashable & Comparable>: CustomStringConvertible {
         return subsetDict.isEmpty
     }
     
-    public var subsets: [Set<T>] {
-        let arr: [Set<T>] = Array(subsetDict.values)
-        return arr.sorted { (first: Set<T>, second: Set<T>) -> Bool in
-            if let fm = first.min(), let sm = second.min() {
-                return fm < sm
-            } else {
-                return false
-            }
-        }
+    public var subsets: Set<Set<T>> {
+        return Set(subsetDict.values)
     }
     
-    public var values: [T] {
-        return Array(keyDict.keys).sorted()
+    public var values: Set<T> {
+        return Set(keyDict.keys)
     }
     
     mutating public func clear() {
@@ -125,19 +103,57 @@ struct DisjointSet<T: Hashable & Comparable>: CustomStringConvertible {
     
 }
 
-struct Location: Hashable, Comparable, CustomStringConvertible {
-    let row: Int
-    let col: Int
+protocol DisjointSetOfComparableConvertible {
+    var customDescription: String { get }
+}
 
-    static func <(lhs: Location, rhs: Location) -> Bool {
-        if lhs.row == lhs.row {
-            return lhs.col < rhs.col
+extension DisjointSet {
+    var normalDescription: String {
+        var description = ""
+        let subs = subsets
+        if subs.count == 0 {
+            description = "Empty"
         } else {
-            return lhs.row < rhs.row
+            description = subs.count == 1 ? "1 subset, " : "\(subs.count) subsets, "
+            description += valueCount == 1 ? "1 value total\n" : "\(valueCount) values total\n"
+            let n = subs.count
+            var i = 0
+            for sub in subs {
+                description += "subset: "
+                description += sub.map { "\($0)" }.sorted().joined(separator: ", ")
+                 if i < n - 1 {
+                    description += "\n"
+                }
+                i += 1
+            }
         }
+        return description
     }
-    
-    var description: String {
-        return "(row: \(row), col: \(col))"
+}
+
+extension DisjointSet: DisjointSetOfComparableConvertible where T: Comparable {
+    var customDescription: String {
+        var description = ""
+        let subs = subsets.sorted { (first: Set<T>, second: Set<T>) -> Bool in
+            if let fm = first.min(), let sm = second.min() {
+                return fm < sm
+            } else {
+                return false
+            }
+        }
+        if subs.count == 0 {
+            description = "Empty"
+        } else {
+            description = subs.count == 1 ? "1 subset, " : "\(subs.count) subsets, "
+            description += valueCount == 1 ? "1 value total\n" : "\(valueCount) values total\n"
+            for i in 0 ..< subs.count {
+                description += "subset: "
+                description += subs[i].sorted().map { "\($0)" }.joined(separator: ", ")
+                if i < subs.count - 1 {
+                    description += "\n"
+                }
+            }
+        }
+        return description
     }
 }
